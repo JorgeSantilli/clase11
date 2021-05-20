6dc9af404a0e4cb28ab85cef2f87ac8eddd41114
Paula Santilli <jorgesantilli1@gamil.com>
2021-05-19 22:29:01 -0300

    SolucionJorge

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-function agregarTarjeta(nombre, precio, imagen) {
-  const card = `<div class="col mb-4 mt-4">
+const productosContainer = document.getElementById('productos-container');
+
+for (let producto of productos) {
+productosContainer.innerHTML += `
+                <div class="col mb-4 mt-4">
                     <a class="card-link" href="http://www.google.com.ar">
                         <div class="card h-100">
-                        <img src="images/${imagen}" class="card-img-top" alt="Producto 1" />
+                        <img src="images/${producto.imagen}" class="card-img-top" alt="Producto 1" />
 
                         <div class="card-body">
-                            <h5 class="card-title">${nombre}</h5>
+                            <h5 class="card-title">${producto.nombre}</h5>
 
                             <p class="card-text">
                             Some quick example text to build on the card title and make up
@@ -14,18 +17,8 @@ function agregarTarjeta(nombre, precio, imagen) {
                         </div>
 
                         <div class="card-footer">
-                            <small class="text-muted">$ ${precio}</small>
+                            <small class="text-muted">$ ${producto.precio}</small>
                         </div>
                         </div>
                     </a>
-                </div>`;
-
-  const productosContainer = document.getElementById('productos-container');
-
-  productosContainer.innerHTML += card;
-}
-
-agregarTarjeta('Producto 1', 1000, 'img3.jpg');
-agregarTarjeta('Producto 2', 2500, 'img2.jpeg');
-agregarTarjeta('Producto 3', 5000, 'img1.webp');
-agregarTarjeta('Producto 4', 160000, 'img3.jpg');
+                </div>`;}
